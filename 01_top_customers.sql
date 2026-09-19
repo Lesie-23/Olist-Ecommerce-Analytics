7253917a587ddc4166833081fde80a2d6b4bbfb3
Lesie Fernandes <lesie0223@gmail.com>
2026-09-19 12:13:06 +0530

    Delete 01_top_customers.sql

--- 01_top_customers.sql
+++
@@ -1,9 +0,0 @@
--- 1 Find the top 5 customers by total payment value.
-select 
-    o.customer_id, sum(p.payment_value) as total_payment 
-from orders o
-inner join order_payments p
-on o.order_id = p.order_id
-group by o.customer_id
-order by total_payment  desc
-limit 5;
